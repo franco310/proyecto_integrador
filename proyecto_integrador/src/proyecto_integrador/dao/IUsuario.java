cfe7da65b58b198f232434a1ef931c8917d9e79e
Usuario <Usuario@192.168.1.100>
2018-06-20 12:42:52 -0500

    MarcoAmancha

--- proyecto_integrador/src/proyecto_integrador/dao/IUsuario.java
+++ proyecto_integrador/src/proyecto_integrador/dao/IUsuario.java
@@ -1,10 +1,14 @@
 
 package proyecto_integrador.dao;
 
-/**
- *
- * @author Usuario
- */
+ import java.util.List;
+import proyecto_integrador.entidades.Usuario;
+
 public interface IUsuario {
+    public int insertar(Usuario usuario) throws Exception;
+    public int modificar(Usuario usuario) throws Exception;
+    public int eliminar(Usuario usuario) throws Exception;
+    public Usuario obtener(int codigo) throws Exception;
+    public List<Usuario> obtener() throws Exception; 
     
 }
